004ce3edb44a66cb4a2f7bdbac1d82b608dd8f49
HP <mintesinotbekele1240@gmail.com>
2024-12-12 10:10:46 +0300

    service tips container

--- src/components/ServiceTipsContainer.jsx
+++ src/components/ServiceTipsContainer.jsx
@@ -6,7 +6,7 @@ import ServiceTips from './ServiceTips';
 
 const ServiceTipsContainer = () => {
   return (
-    <div className='flex md:justify-center gap-8 flex-wrap mt-12 pb-12'>
+    <div className='flex justify-center gap-8 flex-wrap mt-12 pb-12'>
     <ServiceTips icon={<TbTruckDelivery color='white' size={32} />} title='FREE AND FAST DELIVERY' desc='Free delivery for all orders over $140' />
     <ServiceTips icon={<PiHeadset color='white' size={32} />} title='24/7 CUSTOMER SERVICE' desc='Friendly 24/7 customer support' />
     <ServiceTips icon={<VscPass color='white' size={32} />} title='MONEY BACK GUARANTEE' desc='We reurn money within 30 days' />
